b52992f940e716988ccef695149df5006eeec5d8
uzji <else977@aucklanduni.ac.nz>
2023-09-05 23:48:01 +1200

    Add flash effect

--- UI.gd
+++ UI.gd
@@ -1,5 +1,12 @@
 extends Control
 
 
-func _process(_delta:float):
+func _input(event:InputEvent):
+  if event is InputEventMouseButton:
+    if Input.is_action_just_pressed("M2"):
+      $ColorRect.color.a = 1
+
+
+func _process(delta:float):
+  $ColorRect.color.a = lerp($ColorRect.color.a, 0.0, 3*delta)
   $Label.text = str(Engine.get_frames_per_second())
