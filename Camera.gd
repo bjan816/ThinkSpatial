a5314169092fc572829e734071458f52048ec1f4
uzji <else977@aucklanduni.ac.nz>
2023-09-05 23:45:51 +1200

    Update demo

--- Camera.gd
+++ Camera.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 
-@export var faces:Array[Node3D]
+@export var views:Array[Camera3D]
 
 
 func _input(event:InputEvent):
@@ -9,5 +9,5 @@ func _input(event:InputEvent):
     if Input.is_action_pressed("M1"):
       rotation_degrees.x -= event.relative.y
       rotation_degrees.y -= event.relative.x
-      for face in faces:
-        face.rotation = rotation
+      for view in views:
+        view.global_transform = $Camera3D.global_transform
