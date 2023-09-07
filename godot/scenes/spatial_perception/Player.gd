297c230f12a97b8a39ae762a9a4daed88f34e058
uzji <else977@aucklanduni.ac.nz>
2023-09-07 22:10:29 +1200

    Clean up code

--- godot/scenes/spatial_perception/Player.gd
+++ godot/scenes/spatial_perception/Player.gd
@@ -1,44 +1,31 @@
 extends Node3D
 
 
-# READY
-# ——————————————————————————————————————————————————————————————————————————————
-func _ready():
-  query = PhysicsRayQueryParameters3D.create(Vector3.ZERO, Vector3.ZERO)
-
-
 # INPUT
 # ——————————————————————————————————————————————————————————————————————————————
 @export var views:Array[Camera3D]
 @onready var cam:Camera3D = $Camera
 
-var query:PhysicsRayQueryParameters3D
-
 
 func _input(event:InputEvent):
+  # Look
+  if event is InputEventMouseButton and Input.is_action_just_pressed("M1"):
+    # Create query
+    var from = cam.project_ray_origin(event.position)
+    var to   = cam.project_ray_normal(event.position) * 10 + from
+    var query = PhysicsRayQueryParameters3D.create(from, to)
+    # Check query
+    if collide(query): collide(query).collider.check(cam.global_position)
   
-  if event is InputEventMouseButton:
-    if Input.is_action_just_pressed("M1"):
-      query.from = cam.project_ray_origin(event.position)
-      query.to   = cam.project_ray_normal(event.position) * 10 + query.from
-      if collide(query):
-        collide(query).collider.check(cam.global_position)
-  
-  if event is InputEventMouseMotion:
-    if Input.is_action_pressed("M2"):
-      rotation_degrees.x -= event.relative.y
-      rotation_degrees.y -= event.relative.x
-      for view in views:
-        view.global_transform = cam.global_transform
+  # Move
+  if event is InputEventMouseMotion and Input.is_action_pressed("M2"):
+    # Rotate my camera
+    rotation_degrees.x -= event.relative.y
+    rotation_degrees.y -= event.relative.x
+    # Rotate their cameras
+    for view in views: view.global_transform = cam.global_transform
 
 
-func collide(ray_query:PhysicsRayQueryParameters3D):
+func collide(query:PhysicsRayQueryParameters3D):
   var world = get_world_3d().direct_space_state
-  return world.intersect_ray(ray_query)
-
-
-# PROCESS
-# executes at every frame
-# ——————————————————————————————————————————————————————————————————————————————
-func _process(delta):
-  pass
+  return world.intersect_ray(query)
--- godot/scenes/spatial_perception/Puzzle.gd
+++ godot/scenes/spatial_perception/Puzzle.gd
@@ -18,6 +18,6 @@ func _process(delta:float):
 func check(your_cam:Vector3):
   var my_cam:Vector3 = get_node("../Camera").global_position
   if my_cam.distance_to(your_cam) < 1:
-    show()
     for piece in pieces: piece.hide()
+    show()
     animate = true
