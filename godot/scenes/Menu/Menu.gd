a61b5cb2115e2d5c4fd558f4420400b000cd2eba
Jordan <102879780+JordanPisarek@users.noreply.github.com>
2023-09-19 13:07:47 +1200

    added very simple menu to facilitate testing

---
+++ godot/scenes/Menu/Menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _on_borim_pressed():
+	#get_tree().change_scene_to_file()
+	pass
+
+func _on_jamie_pressed():
+	#get_tree().change_scene_to_file()
+	pass
+
+func _on_jordan_pressed():
+	get_tree().change_scene_to_file("res://scenes/Jordan/scenes/Puzzle.tscn")
+
+func _on_sun_pressed():
+	get_tree().change_scene_to_file("res://scenes/spatial_perception/Demo.tscn")
