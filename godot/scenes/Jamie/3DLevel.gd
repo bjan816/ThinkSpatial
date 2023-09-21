5052dae253bd705a139bdf4debf6c80332a95ec7
tlswnsgk12 <jshi136@aucklanduni.ac.nz>
2023-09-22 00:07:06 +1200

    Fixed score error

--- godot/scenes/Jamie/3DLevel.gd
+++ godot/scenes/Jamie/3DLevel.gd
@@ -7,27 +7,28 @@ class_name Game
 @onready var map : = $Map1
 @onready var scoreboard : = $Scoreboard
 
-var level1_scene = load("res://scenes/Jamie/world/Levels/Level1.tscn")
+var levels = ["res://scenes/Jamie/world/Levels/Level1.tscn", "res://scenes/Jamie/world/Levels/Level2.tscn"]
+var level1 = load(levels[0]).instantiate()
+
 var play_time : = 0.0
 var reset_position = Vector3(14.7119, 1.08666, 38.3666)
-var simultaneous_scene = preload("res://scenes/Jamie/world/Levels/Level1.tscn").instantiate()
 var free = false
 
 func _ready():
-	get_tree().root.add_child.call_deferred(simultaneous_scene)
+	get_tree().root.add_child.call_deferred(level1)
 	
 func _process(delta):
 	play_time += delta
 	if Input.is_action_pressed('reset'):	
 		player.global_transform.origin = reset_position
 		player.rotation_degrees = Vector3(0, 90, 0)
-	if simultaneous_scene.player_touched:
+	if level1.player_touched:
 		_on_level_completed()
 
 func _on_level_completed():
 	get_tree().paused = true
-	var total_play_time = play_time
-	final_menu.initialize(total_play_time)
+	final_menu.score = scoreboard.score
+	final_menu.initialize(play_time)
 	timer.hideTimer()
 	scoreboard.hideScore()
 	map.visible = false
