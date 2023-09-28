d55dcce6e075de993fc0bf6ea452e4c7bf7d7316
Jordan <102879780+JordanPisarek@users.noreply.github.com>
2023-09-29 00:26:46 +1300

    changed player camera clamping

--- godot/scenes/Jordan/scripts/Player.gd
+++ godot/scenes/Jordan/scripts/Player.gd
@@ -24,7 +24,7 @@ func _unhandled_input(event):
 		if event is InputEventMouseMotion:
 			neck.rotate_y(-event.relative.x * 0.005)
 			camera.rotate_x(-event.relative.y * 0.005)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
 func _physics_process(delta):
 	# Add the gravity.
