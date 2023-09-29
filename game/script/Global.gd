6d7bf0f15b77a287ba10d3c3c5f4df1f87f20965
uzji <else977@aucklanduni.ac.nz>
2023-09-29 17:27:39 +1300

    Minor code change to return to home

--- game/script/Global.gd
+++ game/script/Global.gd
@@ -3,7 +3,7 @@ extends Node
 
 func _input(event:InputEvent):
   if Input.is_action_just_pressed("ui_accept"):
-    get_tree().change_scene_to_file("res://scene/Home.tscn")
+    to("res://scene/Home.tscn")
 
 
 func to(file:String):
