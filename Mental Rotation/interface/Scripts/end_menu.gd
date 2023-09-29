205d8e926a6ee763b6daffce3c822c6c22fb97c7
ItsAmeKen <kishoratono14@gmail.com>
2023-09-29 19:05:38 +1300

    Moving files to new branch

---
+++ Mental Rotation/interface/Scripts/end_menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+signal retried()
+
+@onready var Finalscore: = $CenterContainer/EndScene/ScoreResults
+
+var score: int = 500
+var ResetButton = false
+
+# Called when the node enters the scene tree for the first time.
+func _initialize():
+	var scoreNum = "Score: %s" % [score]
+	Finalscore.text = scoreNum
+	show()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta) -> void:
+	if Input.is_action_pressed('reset') and !ResetButton:
+		if score > 0:
+			score -= 100
+		ResetButton = true
+	elif !Input.is_action_pressed('reset'):
+		ResetButton = false
+
+
+func _on_try_again_pressed():
+	emit_signal("retried")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
