18d3671780992e065439a1a5035a1e3e2916e673
Jordan <102879780+JordanPisarek@users.noreply.github.com>
2023-10-05 19:38:33 +1300

    changed tutorial box scene and script

--- game/scenes/Jordan/scripts/TutorialBox.gd
+++ game/scenes/Jordan/scripts/TutorialBox.gd
@@ -4,5 +4,6 @@ func _on_video_stream_player_finished():
 	$CenterContainer/Panel/MarginContainer/VBoxContainer/VideoStreamPlayer.play()
 	
 func _on_button_pressed():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	$".".queue_free()
 	get_tree().change_scene_to_file("res://scenes/Jordan/scenes/Puzzle.tscn")
