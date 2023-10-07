44d562b531811f272e430aa8b3ab5c6c4cfaf691
ItsAmeKen <kishoratono14@gmail.com>
2023-10-07 20:06:11 +1300

    fixed

--- Mental Rotation/3dscene.gd
+++ Mental Rotation/3dscene.gd
@@ -2,26 +2,3 @@ extends Node3D
 
 
 
-var targets
-var puzzle_timer
-
-func _ready():
-	set_target()
-	guess()
-	
-func _input(event):
-	if event.is_action_pressed("Click"):
-		var player_choice = guess()
-		if player_choice.is_in_group("target_same"):
-			if player_choice == targets:
-				print("correct") 
-	pass
-
-func guess():
-	var camera = $CharacterBody3D.get_node("FirstPersonNeck/Camera3D")
-	var space = get_world_3d().direct_space_state
-	var mousepos = get_viewport().get_mouse_position()
-	
-
-func set_target():
-	var targets = get_tree().get_nodes_in_group("Targets")
--- Mental Rotation/player/Script/Scriptbullets.gd
+++ Mental Rotation/player/Script/Scriptbullets.gd
@@ -1,4 +1,5 @@
 extends Node3D
+class_name Bullet
 
 const SPEED = 40.0
 
