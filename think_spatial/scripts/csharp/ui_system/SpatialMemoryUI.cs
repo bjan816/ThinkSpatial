fbc6082c751e4e63482780ff7018e2a908ad9f00
bjan816 <bjan816@aucklanduni.ac.nz>
2023-10-20 07:47:32 +1300

    Added Game_Over sound effect

--- think_spatial/scripts/csharp/ui_system/SpatialMemoryUI.cs
+++ think_spatial/scripts/csharp/ui_system/SpatialMemoryUI.cs
@@ -57,10 +57,18 @@ namespace ThinkSpatial.think_spatial.scripts.csharp.ui_system
 			}
 			else if (animationName == CorrectAnimationName)
 			{
+				var soundEffect = GetNode<AudioStreamPlayer>("Correct");
+				soundEffect.Stream = GD.Load<AudioStream>("res://think_spatial/arts/audio/correct.wav");
+				//soundEffect.Play();
+				
 				CorrectAnimationFinished.Send();
 			}
 			else if (animationName == IncorrectAnimationName)
 			{
+				var soundEffect = GetNode<AudioStreamPlayer>("Game_Over");
+				soundEffect.Stream = GD.Load<AudioStream>("res://think_spatial/arts/audio/game_over.wav");
+				soundEffect.Play();
+				
 				IncorrectAnimationFinished.Send();
 			}
 			else if (animationName == ScoreAnimationName)
