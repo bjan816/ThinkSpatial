ce17bffd702448f35262d78a954921a4e10bc344
Jordan <102879780+JordanPisarek@users.noreply.github.com>
2023-10-21 20:41:28 +1300

    changed tutorial box scene for beginner and advanced levels

--- game/scenes/Jordan/scripts/TutorialBox.gd
+++ game/scenes/Jordan/scripts/TutorialBox.gd
@@ -7,3 +7,9 @@ func _on_button_pressed():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	$".".queue_free()
 	get_tree().change_scene_to_file("res://scenes/Jordan/scenes/Puzzle.tscn")
+
+
+func _on_button_2_pressed():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$".".queue_free()
+	get_tree().change_scene_to_file("res://scenes/Jordan/scenes/BeginnerPuzzle.tscn")
